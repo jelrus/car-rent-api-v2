2f7420426f6a535c3b6fe563a729305a199e8717
igor.polianskyi <ipolyanskiy1993@@gmail.com>
2024-12-27 00:14:29 +0200

    add new link

--- frontend/src/utils/axiosInstance.js
+++ frontend/src/utils/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'https://hcxkaw6gl9.execute-api.eu-west-1.amazonaws.com/api/v1',
+  baseURL: 'https://o07xr1faf3.execute-api.eu-west-1.amazonaws.com/api/v1/',
   headers: {
     'Content-Type': 'application/json',
   },
